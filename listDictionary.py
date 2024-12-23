5ef7fe1e246d53567bc55d4149b8671d9a82ccdf
Ben Celsi <benjamin.d.celsi@gmail.com>
2024-12-23 11:50:27 -0800

    sc

--- listDictionary.py
+++ listDictionary.py
@@ -1,19 +1,18 @@
 import os
 
-# List names of photos in /photos into photos.js (or designated directory)
-
-outputFile = "dictionary"
 dictionaryFile = "dictionary.txt"
-photos = []
+outputFile = "dictionary"
+variable = "dictionary"
 
 if os.path.exists(outputFile + ".js"):
     os.remove(outputFile + ".js")
 
 f = open(outputFile + ".js", "a")
-f.write("let " + outputFile + " = new Set([\n")
+f.write("let " + variable + " = new Set([\n")
 with open(dictionaryFile) as file:
     for line in file:
         print(line.rstrip())
         f.write("\"" + line.rstrip() + "\",\n")
 f.write("])")
+
 f.close()
